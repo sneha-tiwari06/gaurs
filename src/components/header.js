a594297ed4b1dacf0c1d3d5d4cb0e22a4ecdc7e5
sneha-tiwari06 <tiwarisneha491@gmail.com>
2024-06-20 18:38:13 +0530

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='banner'>
+        <img src='../assets/1.jpg' alt='Banner' />
+      </div>
+      <div className='navbar'>
+        <div className='logo'>
+          <img src='../assets/logo.jpg' alt='Logo' />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
